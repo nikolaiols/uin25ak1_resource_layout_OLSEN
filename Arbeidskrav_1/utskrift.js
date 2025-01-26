6e4f4d2fe64096ed7a0a8fe8e5013fa00213c0c2
nikolaiols <nikolaio@hiof.no>
2025-01-26 23:50:41 +0100

    fått til å skrive ut fra resources.js

--- Arbeidskrav_1/utskrift.js
+++ Arbeidskrav_1/utskrift.js
@@ -2,13 +2,14 @@
 let HTMLinfo = ""
 resources.map((resource, index) => HTMLinfo =
     ` 
-    <h1>CATEGORY</h1>
-    <p>TEXT</p>
+    <h1>${resources[0].category}</h1>
+    <p>${resources[0].text}</p>
         <ul>
-            <li><a href="#SOURCES.URL">TITLE</a></li>
-            <li><a href="#SOURCES.URL">TITLE</a></li>
-            <li><a href="#SOURCES.URL">TITLE</a></li>
+            <li><a href="${resources[0].sources[0].url}">${resources[0].sources[0].title}</a></li>
+            <li><a href="${resources[1].sources[1].url}">${resources[1].sources[1].title}</a></li>
+            <li><a href="${resources[2].sources[2].url}">${resources[2].sources[2].title}</a></li>
         </ul>
     `
 )
 document.getElementById("print-info").innerHTML = HTMLinfo
+
