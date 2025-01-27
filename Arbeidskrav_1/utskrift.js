f25a491b96b42d00e94d48fd007c1c5f2f0acc1b
nikolaiols <nikolaio@hiof.no>
2025-01-27 23:27:02 +0100

    fikset link problem med .map()

--- Arbeidskrav_1/utskrift.js
+++ Arbeidskrav_1/utskrift.js
@@ -2,22 +2,22 @@
 
 let HTMLinfo = ""
 
-
+//finn ut hvordan du kan bytte ut "html" med hva som blir trykket på!!! og fiks css så det ser likt ut som på skissen
 let resultat = resources.filter((res) => res.category == "HTML")
+
 
 resultat.map(resource => {HTMLinfo +=
     ` 
     <h1>${resource.category}</h1>
     <p>${resource.text}</p>
         <ul>
-            <li><a href="${resource.sources.url}">${resource.sources.title}</a></li>
-            <li><a href="${resource.sources.url}">${resource.sources.title}</a></li>
-            <li><a href="${resource.sources.url}">${resource.sources.title}</a></li>
+            ${resource.sources.map(source => `<li><a href=${source.url}>${source.title}</a></li>`).join("")}
         </ul>
     `
-})
+}) /*må bruke .map for å skrive ut linkene fordi de er lagt inni en array */
 document.getElementById("print-info").innerHTML = HTMLinfo
 
+console.log(resultat)
 /*document.getElementById("bhtml").addEventListener("click", function(){
     const filter = resources.filter(checkresources)
     function checkresources(resource) {
