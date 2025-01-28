440c8678685ba548ee06f7e4849ca65ba968c57e
nikolaiols <nikolaio@hiof.no>
2025-01-28 13:52:17 +0100

    sender med navn til knapp når den starter funksjon

--- Arbeidskrav_1/utskrift.js
+++ Arbeidskrav_1/utskrift.js
@@ -2,18 +2,12 @@
 
 let HTMLinfo = ""
 let BtnName = "HTML" //starter på html siden men kan endres
-let BtnChange = ""
+
 const html = document.getElementById("bhtml")
 
-html.addEventListener("click", () => {
-    BtnChange = `"${html.innerText}"`
-    console.log(BtnName)
-})
-
 //finn ut hvordan du kan bytte ut "html" med hva som blir trykket på!!! og fiks css så det ser likt ut som på skissen
-function Update(){
-
-    
+function Update(CategoryName){
+    BtnName = CategoryName
 let resultat = resources.filter((res) => res.category === BtnName)
 
 resultat.map(resource => {HTMLinfo =
@@ -26,7 +20,6 @@ resultat.map(resource => {HTMLinfo =
     `
 }) /*må bruke .map for å skrive ut linkene fordi de er lagt inni en array */
 document.getElementById("print-info").innerHTML = HTMLinfo}
-Update()
-console.log(Update)
+
 
 
